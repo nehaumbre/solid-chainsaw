a6af234de9508d399cb6108113e615fac9ee0842
nehaumbre <nehaumbre28@gmail.com>
2025-03-04 14:09:17 +0530

    Data Types

---
+++ VAR_DT_M_02/Datatypes/script.js
@@ -0,0 +1,25 @@
+//DataTypes
+/** 
+ * Strings : Sequence of characters inside "" /''/ backticks   
+ * Numbers : Integers, floating point numbers
+ * Boolean : true/false 0/1
+ * Null :Intentional absence of any objevt value
+ * Undefined : a variable that has not been yet defined/ assigned any value
+ * Symbol : Built in object whose constructor returns a unique symbol 
+ * BigInt :Numbers greater than 'Number' Type can handle
+ * 
+ * **/
+//Reference types /objects
+//they are non primitive value
+//when assigned to a variable, this variable is given a reference to that value
+//object literals , arrays and fuctions are all reference types
+
+/*Static v/s Dynamic Types
+-JS is dynamically typed language (we do not explicitly define types for our variables)
+-ex of statically typed languages: C C++ Java
+Typescript is superset of JS which allows static typing making code verbose and less prone to errors 
+
+
+
+
+*/
